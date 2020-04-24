9ebc1162a50a3f34b408e64dbfd62ce349ef7f00
Muhammad Husain Giffary Alsaera <muhammadhusain1996@gmail.com>
2020-04-24 20:35:52 +0700

    repaire crash endpoint

--- api.py
+++ api.py
@@ -78,4 +78,5 @@ def Facereco():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT',5000))
+    app.run(debug=True, port=port)
