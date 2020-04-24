d38bd9b9536cd6a07a3aadce0be653d4b7f39451
Muhammad Husain Giffary Alsaera <muhammadhusain1996@gmail.com>
2020-04-24 21:12:20 +0700

    repaire endpoint again

--- api.py
+++ api.py
@@ -78,5 +78,5 @@ def Facereco():
     return jsonify(response)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT',5000))
+    app.run(debug=True, port=5005)
